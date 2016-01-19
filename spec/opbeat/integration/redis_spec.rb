d1df7eb2e3c5fe7bcae0728401b8386a86a45073
Mikkel Malmberg <mikkel@brnbw.com>
2016-01-19 12:15:12 +0100

    Only run redis spec when a redis is available

--- spec/opbeat/integration/redis_spec.rb
+++ spec/opbeat/integration/redis_spec.rb
@@ -1,19 +1,27 @@
 require 'spec_helper'
-require 'mock_redis'
 
-RSpec.describe "Redis integration", start_without_worker: true do
+begin
+  require 'mock_redis'
+  enabled = true
+rescue LoadError
+  enabled = false
+end
 
-  let(:redis) { Redis.new }
+if enabled
+  RSpec.describe "Redis integration", start_without_worker: true do
 
-  describe "#call" do
-    it "adds a trace to current transaction" do
-      transaction = Opbeat.transaction 'Redis' do
-        redis.lrange("some:where", 0, -1)
+    let(:redis) { Redis.new }
+
+    describe "#call" do
+      it "adds a trace to current transaction" do
+        transaction = Opbeat.transaction 'Redis' do
+          redis.lrange("some:where", 0, -1)
+        end
+
+        expect(transaction.traces.length).to be 2
+        expect(transaction.traces.last.signature).to eq 'lrange'
       end
-
-      expect(transaction.traces.length).to be 2
-      expect(transaction.traces.last.signature).to eq 'lrange'
     end
-  end
 
+  end
 end
